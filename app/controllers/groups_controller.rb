74bf4491d3f9964e949e9489539c9fa0e38985e9
Dmitriy Pervin <theforner@gmail.com>
2015-09-22 21:04:33 +0500

    Remove session check in schedule

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,11 +4,7 @@ class GroupsController < ApplicationController
   end
 
   def show
-    if session? and !params.has_key?(:to)
-      @session = true
-      @from = Date.today
-      @to = Date.today+30.days
-    elsif params.has_key?(:from) and params.has_key?(:to)
+    if params.has_key?(:from) and params.has_key?(:to)
       @from = Date.parse(params[:from])
       @to = Date.parse(params[:to])
     else
@@ -18,14 +14,6 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
     if stale?([@group, @from, @to], public: true)
       @schedule = Entity.order(:start).where(group_id: @group, start: @from..@to).eager_load(:teacher, :subject, :auditorium, :entity_type)
-    end
-  end
-
-  def session?
-    if Date.today < DateTime.new(2014, 02, 01)
-      return true
-    else
-      return false
     end
   end
 end
