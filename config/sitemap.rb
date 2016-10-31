f8140396f4184daa0c36564649dba849793e98bf
Dmitriy Pervin <theforner@gmail.com>
2016-10-31 18:23:22 +0500

    Change sitemap generator

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,15 +1,19 @@
-SitemapGenerator::Sitemap.default_host = 'http://schedule.cravs.com'
+SitemapGenerator::Sitemap.default_host = 'https://schedule.cravs.com'
 
 SitemapGenerator::Sitemap.create do
-  add groups_path, :priority => 1, :changefreq => 'daily'
+  add groups_path, :priority => 1, :changefreq => 'weekly'
+  add teachers_path, :priority => 1, :changefreq => 'weekly'
+  add auditoriums_path, :priority => 0.9, :changefreq => 'weekly'
 
-  Group.find_each do |group|
-    add group_path(group), :lastmod => group.updated_at
+  Group.where(archive: false).find_each do |group|
+    add group_path(group), :lastmod => group.updated_at, :priority => 0.8, :changefreq => 'daily'
   end
 
-  add teachers_path, :priority => 1, :changefreq => 'daily'
-
   Teacher.find_each do |teacher|
-    add teacher_path(teacher)
+    add teacher_path(teacher), :priority => 0.7, :changefreq => 'daily'
+  end
+
+  Auditorium.find_each do |aud|
+    add auditorium_path(aud), :priority => 0.6, :changefreq => 'daily'
   end
 end
